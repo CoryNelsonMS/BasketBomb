52dc2ebfd544b4449c4f81f2e252e221a0d9fe02
Cory Nelson <CoryNelsonMS@outlook.com>
2015-08-15 01:48:36 +0100

    initial commit

---
+++ src/com/basketbomb/BasketBomb.java
@@ -0,0 +1,15 @@
+package com.basketbomb;
+
+/**
+ * Created by CoryN on 15/08/2015.
+ */
+public class BasketBomb {
+
+    public static void main(String[] args) {
+        new BasketBomb();
+    }
+
+    public BasketBomb() {
+        System.out.println("");
+    }
+}
